ebd0691377ee16f078e9f106c16121aaee368f51
ChazdsterDazster <charlotte.deane2@education.nsw.gov.au>
2025-02-18 00:58:51 +0000

    commit

--- src/lesson_0/indoor.py
+++ src/lesson_0/indoor.py
@@ -1,6 +1,8 @@
 def indoor():
     # This code is an example and should be replaced with your own solution
-    print("Hello, world!")
+    value = input()
+    print(value.lower())
+
 
 if __name__ == "__main__":
     indoor()
