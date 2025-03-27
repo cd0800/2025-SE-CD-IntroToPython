7b64b73b44406f02722c2485204f49782c113d52
ChazdsterDazster <charlotte.deane2@education.nsw.gov.au>
2025-03-27 02:14:18 +0000

    faces

---
+++ src/function_variables/einstein.py
@@ -0,0 +1,4 @@
+mass = int(input("Enter the mass of the object in kilograms "))
+c = 300000000
+relativity = mass * c**2
+print(relativity)
---
+++ src/function_variables/faces.py
@@ -0,0 +1,9 @@
+def convert(emoji):
+    if emoji == ":)":
+        print("🙂")
+    if emoji == ":(":
+        print("🙁")
+
+
+a = input("What emoticon would you like to convert? ")
+convert(a)
